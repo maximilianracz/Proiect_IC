07ae3b0e86fc66d5ce51c9e4d366e7370d2705d4
maximilianracz <maximilian.racz@student.upt.ro>
2025-04-01 02:06:28 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Meniu from "./pages/Meniu";
+import Harta from "./pages/Harta";
+import AdaugaCerere from "./pages/AdaugaCerere";
+import TopDonatori from "./pages/TopDonatori";
+import DonariDeschise from "./pages/DonariDeschise";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+
+        {/* Toate paginile de mai jos sunt protejate */}
+        <Route
+          path="/meniu"
+          element={
+            <ProtectedRoute>
+              <Meniu />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/harta"
+          element={
+            <ProtectedRoute>
+              <Harta />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/adauga-cerere"
+          element={
+            <ProtectedRoute>
+              <AdaugaCerere />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/top-donatori"
+          element={
+            <ProtectedRoute>
+              <TopDonatori />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/donari-deschise"
+          element={
+            <ProtectedRoute>
+              <DonariDeschise />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
